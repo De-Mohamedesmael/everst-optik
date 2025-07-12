3abf918a9486de66746ebf3a42e6bba710b44d38
tariksalah <tariksalahnet@hotmail.com>
2025-07-12 17:38:20 +0300

    add: sell lenses to uts

--- Modules/Factory/Resources/views/back-end/lenses/index.blade.php
+++ Modules/Factory/Resources/views/back-end/lenses/index.blade.php
@@ -145,9 +145,11 @@
                                     <thead>
                                         <tr>
                                             <th>@lang('lang.factory_name')</th>
-                                            <th>@lang('lang.product')</th>
+                                            <th>@lang('lang.products')</th>
                                             <th>@lang('lang.date')</th>
-                                            <th class="notexport">@lang('lang.action')</th>
+                                            <th>@lang('qrcode')</th>
+                                            <th></th>
+                                            <th></th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -155,6 +157,8 @@
                                     </tbody>
                                     <tfoot>
                                         <tr>
+                                            <td></td>
+                                            <td></td>
                                             <td></td>
                                             <td></td>
                                             <td></td>
@@ -212,12 +216,10 @@
                     }
                 },
                 columnDefs: [{
-                        // "targets": [0,2, 3],
                         "orderable": true,
                         "searchable": true
                     },
                     {
-                        "targets": [6], // Column 6 (purchases)
                         "orderable": true, // Enable sorting for the "purchases" column
                         "searchable": true
                     }
@@ -235,28 +237,25 @@
                         name: 'date'
                     },                    
                     {
-                        data: 'action',
-                        name: 'action'
+                        data: 'scan_input',
+                        name: 'scan_input',
+                        orderable: false,
+                        searchable: false
                     },
-
+                    { 
+                        data: 'qr_code_image', 
+                        name: 'qr_code_image', 
+                        orderable: false, 
+                        searchable: false 
+                    },
+                    { 
+                        data: 'actions', 
+                        name: 'actions', 
+                        orderable: false, 
+                        searchable: false 
+                    }                                        
                 ],
-                createdRow: function(row, data, dataIndex) {
-
-                },
-                fnDrawCallback: function(oSettings) {
-                    var intVal = function(i) {
-                        return typeof i === "string" ?
-                            i.replace(/[\$,]/g, "") * 1 :
-                            typeof i === "number" ?
-                            i :
-                            0;
-                    };
-
-
-
-                },
             });
-
         });
 
 
@@ -351,9 +350,11 @@
 
 
         });
+
         $(document).on('click', '.filter_product', function() {
             store_table.ajax.reload();
         })
+
         $(document).on('click', '.clear_filters', function(e) {
             // e.preventDefault();
             $('#startdate').val('');
@@ -361,5 +362,98 @@
             $('#customer_type_id').val('')
             store_table.ajax.reload();
         });
+
+        // save qrcode
+        $(document).on('change', '.scan-input', function () {
+            let qr_value = $(this).val();
+            let id = $(this).data('id');
+
+            if (!qr_value) return;
+
+  
+            $.ajax({
+                url: '/dashboard/factories/lenses/save-qr',
+                method: 'POST',
+                data: {
+                    _token: $('meta[name="csrf-token"]').attr('content'),
+                    id: id,
+                    qr_code: qr_value
+                },
+                success: function (response) {
+                    if (response.status === 'success') {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'تم الحفظ بنجاح',
+                            text: response.message || '',
+                            timer: 2000,
+                            showConfirmButton: false
+                        });
+                    }
+
+                    // refresh the table
+                    // store_table.ajax.reload(null, false);
+                },
+                error: function (xhr) {
+                    if (xhr.status === 422) {
+                        let errors = xhr.responseJSON.errors;
+                        let errorMessages = Object.values(errors).map(e => e[0]).join('<br>');
+
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'خطأ في التحقق',
+                            html: errorMessages
+                        });
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'خطأ غير متوقع',
+                            text: 'حدث خطأ أثناء الحفظ، يرجى المحاولة مرة أخرى'
+                        });
+                    }
+                }
+            });
+        });
+
+        // sell via uts
+        $(document).on('click', '.send-btn', function () {
+            const id = $(this).data('id');
+            const input = $('.scan-input[data-id="' + id + '"]').val();
+
+            if (!input) {
+                alert('يرجى إدخال القيمة أولاً');
+                return;
+            }
+
+            $.ajax({
+                url: '/dashboard/factories/lenses/sell_to_uts',
+                type: 'POST',
+                data: {
+                    uno: input,
+                    lot: 'LOT20250701',
+                    adt: 1,
+                    vrn: '2025-07-12',
+                    vrnT: 'SATIS',
+                    aciklama: 'إرسال يدوي',
+                    aliciKurum: {
+                        krn: '09876543210987',
+                        tip: 'FIRMA'
+                    }
+                },
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                success: function (response) {
+                    console.log(response);
+                    alert('تم الإرسال بنجاح');
+                },
+                error: function (xhr) {
+                    console.error(xhr.responseText);
+                    alert('حدث خطأ أثناء الإرسال');
+                }
+            });
+        });
+
+
+
     </script>
 @endsection
--- Modules/Factory/Routes/web.php
+++ Modules/Factory/Routes/web.php
@@ -8,5 +8,7 @@ Route::group(['middleware' => ['auth:admin', 'SetSessionData', 'language', 'time
 
     Route::get('factories/lenses/index', [FactoryController::class, 'getFactoriesLenses'])->name('factories.lenses.index');
     Route::get('factories/lenses/create', [FactoryController::class, 'createLenses'])->name('factories.lenses.create');
-    Route::post('factories/lenses/send', [FactoryController::class, 'sendLenses'])->name('factories.lenses.send');
+    Route::post('factories/lenses/save-qr', [FactoryController::class, 'saveQr'])->name('factories.lenses.save-qr');
+    Route::post('factories/lenses/sell_to_uts', [FactoryController::class, 'sellToUts'])->name('factories.lenses.sell_to_uts'); // send to uts
+    
 });
